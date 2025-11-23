0002b091f06a56b4f8f6524363d260403c3e7118
haning47 <155292432+haning47@users.noreply.github.com>
2025-11-23 22:09:31 +0800

    0-

--- testpano/data.js
+++ testpano/data.js
@@ -1,33 +1,6 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-",
-      "name": "校門口北側",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
       "id": "1-",
       "name": "校門口草坪",
       "levels": [

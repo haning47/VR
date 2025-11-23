7131a86ab0f4b08f1d243708fac06b47a38f9546
haning47 <155292432+haning47@users.noreply.github.com>
2025-11-23 22:13:19 +0800

    3-2

--- testpano/data.js
+++ testpano/data.js
@@ -54,33 +54,7 @@ var APP_DATA = {
       "linkHotspots": [],
       "infoHotspots": []
     },
-    {
-      "id": "3-2",
-      "name": "圖書館2樓",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
+    
     {
       "id": "4-",
       "name": "往藝能大樓",

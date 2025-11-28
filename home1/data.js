2876f15f1140f698fb82116247a79837d605ab15
haning47 <155292432+haning47@users.noreply.github.com>
2025-11-28 10:49:57 +0800

    home

--- home1/data.js
+++ home1/data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-",
-      "name": "校門口",
+      "name": "松山高中",
       "levels": [
         {
           "tileSize": 256,
@@ -16,23 +16,38 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 4096,
       "initialViewParameters": {
-        "yaw": -1.5292861294303144,
-        "pitch": 0.20314959234160668,
-        "fov": 1.2599180821480807
+        "yaw": 2.9727764557396776,
+        "pitch": -0.23053581739467077,
+        "fov": 1.0025330419681413
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -2.973940907161076,
+          "pitch": -0.27397618161354487,
+          "title": "松山高中簡介",
+          "text": "前身為臺北市松山初級中學<div>民國78年改制為高中</div><div>校慶為 11月12日</div><div>坐落在捷運市政府站附近</div><div>交通便利</div>"
+        }
+      ]
     }
   ],
-  "name": "松高",
+  "name": "松山高中720環景",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
